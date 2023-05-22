f1c2a2cb6f5e1c26af2b29dd8e2caac886d62e53
Rafael Moura <rafael.g.moura@hotmail.com>
2023-05-22 15:52:43 -0300

    02-03 Faça como eu fiz: Inserindo Views na Activity

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+
+        view.setText("Cesta de Frutas")
+        setContentView(view)
+
+
     }
 }
