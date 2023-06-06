f04a91eb0012185e79bcc8566211b7184dcbd991
Rafael Moura <rafael.g.moura@hotmail.com>
2023-06-06 12:24:22 -0300

    01-09 Faça como eu fiz: Adicionando imagens no projeto

--- app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
@@ -1,21 +1,24 @@
 package com.example.orgs.dao
 
 import com.example.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         Companion.produtos.add(produto)
     }
 
 
-    fun buscaTodos():List<Produto>{
+    fun buscaTodos(): List<Produto> {
 
 
         return Companion.produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto("Salada de Fruta", "Laranja, Maçã e Uva", BigDecimal("19.83"))
+        )
     }
 }
